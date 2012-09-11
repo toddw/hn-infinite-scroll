d29824fee1b8d2eaa59e6d78ca0de0c95db6668d
Todd Williams <toddw@media1designs.com>
2012-09-10 23:01:57 -0700

    added support for larger screens

--- infinite-scroll.js
+++ infinite-scroll.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
   $("body").append('<div id="loader">loading...</div>');
   
+  function isLoaderOnScreen() {
+    return $(window).scrollTop() + window.innerHeight + 500 > $("#loader").offset().top
+  }
+
   function loadNext() {
     var nextPageHolderId = "page-" + Date.now(),
       moreButton = $("a:contains('More'):last"),
@@ -16,12 +20,16 @@ $(document).ready(function() {
     $("#" + nextPageHolderId).load(nextPageURL, function() {
       $("#" + nextPageHolderId).find('tr:first').remove();
     });
+
+    if(isLoaderOnScreen()) {
+      loadNext();
+    }
   }
   
   $(document).scroll(function() {
     clearTimeout(window.scrollTimeout);
     window.scrollTimeout = setTimeout(function() {
-      if($(window).scrollTop() + window.innerHeight + 500 > $("#loader").offset().top) {
+      if(isLoaderOnScreen()) {
         loadNext();
       }
     }, 500);
